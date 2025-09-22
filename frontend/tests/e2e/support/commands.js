ce15ddec7f45ae975134f964822857332424945b
Alo Holmes <95767456+Alo-Holmes@users.noreply.github.com>
2025-09-22 13:21:43 +0200

    cleaning the project

--- frontend/tests/e2e/support/commands.js
+++
@@ -1,7 +0,0 @@
-// Custom Cypress commands for MiniMart testing
-
-// Prevent Cypress from failing on uncaught exceptions that might occur in the app
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
